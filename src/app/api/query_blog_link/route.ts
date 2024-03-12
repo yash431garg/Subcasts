787476b3df6b48d9de5e2823b988aee35be59b69
Yash Garg <yash431garg@gmail.com>
2024-03-12 15:54:27 +0530

    updated query_blog_link api

--- src/app/api/query_blog_link/route.ts
+++ src/app/api/query_blog_link/route.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import OpenAI from 'openai';
-
 import { parse } from 'node-html-parser';
 import * as cheerio from 'cheerio';
 import { NextRequest, NextResponse } from 'next/server';
@@ -9,7 +8,7 @@ import { getFrameHtmlResponse } from '@coinbase/onchainkit';
 
 export async function POST(req: NextRequest) {
   try {
-    const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
+    // const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
     const data = await req.json(); // This contains the data sent in the POST request
 
     const response = await fetch(data);
@@ -21,7 +20,7 @@ export async function POST(req: NextRequest) {
     const $ = cheerio.load(html);
 
     const imageLink = $('meta[property="og:image"]').attr('content');
-    const description = $('meta[property="og:description"]').attr('content');
+    // const description = $('meta[property="og:description"]').attr('content');
 
     // Parse HTML using node-html-parser
     // Remove internal style and script tags
@@ -35,11 +34,11 @@ export async function POST(req: NextRequest) {
     const textContent = $('body').text();
     // console.log(textContent);
 
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: 'system', content: `Summarize: ${textContent}` }],
-      model: 'gpt-3.5-turbo',
-    });
-    console.log(completion.choices[0].message.content);
+    // const completion = await openai.chat.completions.create({
+    //   messages: [{ role: 'system', content: `Summarize: ${textContent}` }],
+    //   model: 'gpt-3.5-turbo',
+    // });
+    // console.log(completion.choices[0].message.content);
     return NextResponse.json({
       img: imageLink,
       description: '',

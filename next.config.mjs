6484662cd67f82d9687d46ed8fb4245b6a70b7bf
Yash Garg <yash431garg@gmail.com>
2024-03-12 16:01:31 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '',
-        port: '',
-        pathname: '',
+        // hostname: '',
+        // port: '',
+        // pathname: '',
       },
     ],
   },

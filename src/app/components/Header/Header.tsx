dbf19c1b2c4331d49b38f67bceec364dfe601193
Yash Garg <yash431garg@gmail.com>
2024-03-22 17:11:15 +0530

    updated auth code

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react"
 
 export default function Header() {
-    const { data: session, status } = useSession()
+    // const { data: session, status } = useSession()
 
     return (
         <div className={classname.header}>
@@ -17,9 +17,7 @@ export default function Header() {
                 <button onClick={() => signOut()} className='text-red-600 border-1 p-2 rounded border-red-600'>
                     Logout
                 </button>
-                {session?.user.image && (
-                    <img className="w-8 h-8 md:w-10 md:h-10 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src={session.user.image} alt="Bordered avatar" />
-                )}
+
             </div>
         </div>
     )
